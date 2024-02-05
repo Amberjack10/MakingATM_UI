d033711995b8751ad55b1908e064bd5287c3fc02
Amberjack10 <sanghwasong10@gmail.com>
2024-02-05 19:05:10 +0900

    <feat> : 잔액 부족 팝업 추가

---
+++ Assets/Scripts/WarningPopup.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningPopup : MonoBehaviour
+{
+    public void OnClickOKButton()
+    {
+        gameObject.SetActive(false);
+    }
+}
